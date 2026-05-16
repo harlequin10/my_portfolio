1ddf87d9f0715ed23c0c54bbcbea22ca606866b6
harlequin10 <117296460+harlequin10@users.noreply.github.com>
2026-05-16 20:02:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
+
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
 
 function openLightbox(src) {
-  document.getElementById('lightbox').style.display = 'block';
-  document.getElementById('lightbox-img').src = src;
+  document.getElementById("lightbox").style.display = "block";
+  document.getElementById("lightbox-img").src = src;
 }
 
 function closeLightbox() {
-  document.getElementById('lightbox').style.display = 'none';
+  document.getElementById("lightbox").style.display = "none";
 }
